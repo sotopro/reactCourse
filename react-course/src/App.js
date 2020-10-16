74bbc8055daf944c72de3fefe7e86e09daaef3d2
Daniel Soto <dasg.1993@gmail.comm>
2020-10-16 18:52:49 -0500

    [feature] - create product component like cart product

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,43 +1,20 @@
 import React, { Component } from 'react';
-import Title from './components/Title'
-import Button from './components/Button'
-import data from './utils/data';
-import Card from './components/Card'
+import Product from './components/Product/'
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      buttonText: 'Continuar'
-    }
-  }
-  handleClick = () => {
-    this.setState({
-      buttonText: 'Gracias por hacer Click'
-    })
-  }
-  showAdditional = (additional) => {
-    const aletInformation = Object.entries(additional)
-    .map(information => `${information[0]} : ${information[1]}`)
-    .join('\n');
-    alert(aletInformation)
+    this.state = {}
   }
   render(){
-    const { buttonText} = this.state;
   return (
-    <div className="App">
-      <h2>Bienvenidos</h2>
-      <Title text='Curso de React Js'/>
-      <Button text={buttonText} onClickHandle={this.handleClick}/>
-      <div className='wrapper'>
-      {data.map(animal => (
-        <Card showAdditional={this.showAdditional} key={animal.name} name={animal.name} additional={animal.additional} diet={animal.diet} scientificName={animal.scientificName} size={animal.size}/>
-      ))}
+      <div>
+          <Product />
       </div>
-    </div>
-  );
-}
+  )
+
+	}
 }
 
 export default App;
