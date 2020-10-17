3ec03eec2c54753be9b25136df7d7418a2aacc27
Daniel Soto <dasg.1993@gmail.comm>
2020-10-16 19:39:41 -0500

    [feature] - product hook component & reducer

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Product from './components/Product/'
+import Product from './components/ProductHook'
 import './App.css';
 
 class App extends Component {
